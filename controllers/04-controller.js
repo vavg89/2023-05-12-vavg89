7b1f54f47790b5156ef8dcea868498ee5a5cfb70
vavg89 <vavg89.ar@gmail.com>
2023-05-13 00:33:18 -0300

    resolve test 01,02,03,04,07,08,09,10

--- controllers/04-controller.js
+++ controllers/04-controller.js
@@ -8,5 +8,10 @@ Busca un animal por ID. Elimínalo de la base de datos y responde con el array d
 Si no existe un animal con ese ID, arrojá un error que diga: `No existe el animal con id: ${animalId}`.
 */
 
-const deleteAnimal = (animalId) => {};
+const deleteAnimal = (animalId) => {
+    if (!utils.animal.find(ani => ani.id === animalId)) throw Error(`No existe el animal con id: ${animalId}`)
+let animales = utils.animal.filter(ani => ani.id !== animalId)
+utils.animal = animales
+return animales  
+};
 module.exports = deleteAnimal;
--- routes/10-route.js
+++ routes/10-route.js
@@ -16,7 +16,16 @@ const deleteAnimal = require("../controllers/04-controller");
   3) Revisa también los tests para saber por dónde vendrá el id del animal que queremos eliminar
 */
 
-router.delete("/animal", (req, res) => {});
+router.delete("/animal", (req, res) => {
+  const id = req.body.id
+  
+  try {
+    deleteAnimal(id)
+    res.status(200).json({message: `El animal con el id ${id} fue eliminado de forma exitosa`})
+  } catch (error) {
+    res.status(400).json({error: error.message})
+  }
+});
 
 //⚠️ No modificar nada debajo de esta línea ⚠️
 module.exports = router;
